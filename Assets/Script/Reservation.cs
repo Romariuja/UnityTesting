62062de846abddd0d3d89908496c993e909b974c
ID6 <ID6@daisalux.com>
2020-08-26 14:55:47 +0200

    initial commit

---
+++ Assets/Script/Reservation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reservation : MonoBehaviour
+{
+  
+    public User MadeBy { get; set; }
+    public bool CanBeCancellledBy(User user)
+    {
+        if (user.IsAdmin)
+            return true;
+        if (MadeBy == user)
+            return true;
+        return false;
+
+    }
+
+}
+
+public class User
+{
+    public bool IsAdmin { get;  set;}
+}
---
+++ Assets/Tests/PlayMode/NewTestScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+
+            //Act
+
+            var result = reservation.CanBeCancellledBy(new User { IsAdmin = true });
+
+            //Assert
+
+
+            Assert.IsTrue(result);
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
